e86ad8f463cbaf49b0bab21a815930e150740359
cektop7 <cektop7@gmail.com>
2019-05-05 14:57:48 +0300

    Release

---
+++ src/driver/json.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @Description: Создано в России 
+ * @User: aleksey.nikulin
+ * @Date: 16.08.2016
+ * @Time: 22:34
+ * @Email: masterweb@e1.ru
+ */
+
+namespace BoxberryApi\Json;
+
+class Json {
+
+    /**
+     * @var array
+     */
+    public $args = [];
+
+    /**
+     * @var string
+     */
+    private $methodQuery = 'post';
+
+    /**
+     * @var array
+     */
+    public $error = [];
+
+    const ResNotData = "Response not data";
+
+    /**
+     * @param string $method
+     * @param array $args
+     */
+    function __construct($method = '', $args = [])
+    {
+        $args['method'] = $method;
+        $this->args = $args;
+    }
+
+    /**
+     * @param string $data
+     * @return mixed
+     */
+    function toArray($data='')
+    {
+        return json_decode($data, 1);
+    }
+
+    /**
+     * @return $this
+     */
+    public function setMethodQuery(){
+        if(isset($this->args['methodQuery'])){
+            if(in_array($this->args['methodQuery'],['post','get'])) {
+                $this->methodQuery = $this->args['methodQuery'];
+            }
+            unset($this->args['methodQuery']);
+        }
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    function Request()
+    {
+        try{
+            $this->connect();
+            $methodQuery = $this->setMethodQuery()->methodQuery;
+            if($data = (($methodQuery == 'get')?
+                \Requests::$methodQuery(HOST. join("?",array_diff([JSON_SERVICE, http_build_query($this->args)],[""])))
+                : \Requests::$methodQuery(HOST.JSON_SERVICE,[],$this->args))){
+                return $this->toArray($data->body);
+            } else {
+                $this->error[] = self::ResNotData;
+            }
+        } catch (\Exception $e){
+            $this->error[] = $e->getMessage();
+        }
+    }
+
+    function connect(){
+        require_once(BOXBERRY_DIR.'driver/Request/library/Requests.php');
+        \Requests::register_autoloader();
+    }
+}
---
+++ src/driver/soap.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @Description: Создано в России 
+ * @User: aleksey.nikulin
+ * @Date: 27.06.2016
+ * @Time: 21:29
+ * @Email: masterweb@e1.ru
+ */
+
+namespace BoxberryApi\Soap;
+
+    class Soap {
+        /**
+         * @var string
+         */
+        public $method = '';
+
+        /**
+         * @var array
+         */
+        public $args = [];
+
+        /**
+         * @var array
+         */
+        public $error = [];
+
+        /**
+         * @var array
+         */
+        public $soap = [];
+
+        /**
+         * @param string $method
+         * @param array $args
+         */
+        function __construct($method = '', $args = [])
+        {
+            $this->method = $method;
+            $this->args = $args;
+        }
+
+        /**
+         * @param \stdClass $data
+         * @return mixed
+         */
+        function toArray(\stdClass $data)
+        {
+            return json_decode(json_encode($data), 1);
+        }
+
+        /**
+         * @return mixed
+         */
+        function Request()
+        {
+            if ($client = $this->connect()) {
+                try {
+                    $method = $this->method;
+                    return $this->toArray($client->$method($this->args));
+                } catch (\SoapFault $e) {
+                    $this->error[] = $e->getMessage();
+                }
+            }
+        }
+
+        /**
+         * @return \SoapClient
+         */
+        private function connect()
+        {
+            try {
+                return new \SoapClient(HOST.$this->setWsdlDocument());
+            } catch (\SoapFault $e) {
+                $this->error[] = $e->getMessage();
+            }
+        }
+
+        /**
+         * @return int|string
+         */
+        private function setWsdlDocument()
+        {
+            if (defined("SOAP")) {
+                $this->soap = json_decode(SOAP, 1);
+                foreach ($this->soap as $k => $a) {
+                    if (in_array($this->method, $a)) {
+                        return $k;
+                    }
+                }
+                return (string)array_pop(array_keys($this->soap));
+            }
+        }
+    }
